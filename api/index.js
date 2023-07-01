8522c0868fd080ec6d4a551d8e53babf0c12a570
CDRojasRamirez <cdanielrap@gmail.com>
2023-07-01 00:37:50 -0500

    detalles de deploy

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const PORT = process.env.PORT || 3005;
+
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3005, () => {
-    console.log('%s listening at 3005'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
   });
 });
